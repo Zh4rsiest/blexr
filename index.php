6e848d5c94da64b22932ff538f1f21442cc09bbb
Zh4rsiest <baloghzsombor94@gmail.com>
2020-11-13 09:46:48 +0100

    Removed commented code from index.php

--- index.php
+++ index.php
@@ -3,37 +3,6 @@ require("vendor/autoload.php");
 require("routes/web.php");
 use App\Controllers\AuthController as AuthController;
 use App\Models\Auth as Auth;
-// echo "<pre>";
-// print_r(get_defined_constants());
-// echo "</pre>";
-
-// echo "<pre>";
-// print_r(get_declared_classes());
-// echo "</pre>";
-
-// $user = new \App\Models\User();
-// $_tmp = $user->create([
-//   "name" => "Jack Nicholson",
-//   "email" => "employee@blexr.com",
-//   "role" => 2
-//   // "password_length" => 8
-// ]);
-// print_r($_tmp);
-
-// $auth = new AuthController();
-// $auth->login([
-//   "email" => "test@asd.com",
-//   "password" => "asd"
-// ]);
-// echo "<pre>";
-// print_r(json_encode(Auth::user()));
-// echo "</pre>";
-
-// $user = new \App\Models\User();
-//
-// echo "<pre>";
-// print_r($user->all());
-// echo "</pre>";
 
 $uri = ltrim($_SERVER["REQUEST_URI"], "/");
 
